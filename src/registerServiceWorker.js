ac85fe6268baaf1c611c96ff14225a67b82bee48
jhonatanborg <jhonatanborgesdj@gmail.com>
2020-10-02 16:07:19 -0400

    releases sw

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -3,7 +3,7 @@
 import { register } from "register-service-worker";
 
 if (process.env.NODE_ENV === "production") {
-  register(`${process.env.BASE_URL}service-worker.js`, {
+  register(`${process.env.BASE_URL}sw.js`, {
     ready() {
       console.log(
         "App is being served from cache by a service worker.\n" +
@@ -29,6 +29,6 @@ if (process.env.NODE_ENV === "production") {
     },
     error(error) {
       console.error("Error during service worker registration:", error);
-    }
+    },
   });
 }
